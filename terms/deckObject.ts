4c5e49911d4fad4ac846dd3f77a9e925b0ef5733
NadyaCodes <nadyacorscadden@gmail.com>
2024-08-31 22:34:54 -0400

    create basic flash game

--- terms/deckObject.ts
+++ terms/deckObject.ts
@@ -1,7 +1,5 @@
 import { directionalTerms, introTerms } from "./basics";
-import { tissueTerms, tissueTerms2, tissueTerms3, tissueTerms4 } from "./tissues";
-import { basicAnatomicalTerms, cellParts, microscopeTerms, tissueTermsX } from "./anatomyX";
-import { nervousTerms, nervousTerms2 } from "./nervous";
+
 export type TermObject = {
   [key: string]: string;
 };
@@ -43,74 +41,5 @@ export const deckObject: DeckObject = {
     unitName: "Directional Terms",
     data: directionalTerms
   },
-  "2": {
-    group: "tissues",
-    chapter: "Chapter 2",
-    unitCode: "tissues",
-    unitName: "Tissue Terms 1",
-    data: tissueTerms
-  },
-  "3": {
-    group: "tissues",
-    chapter: "Chapter 2",
-    unitCode: "tissues2",
-    unitName: "Tissue Terms 2",
-    data: tissueTerms2
-  },
-  "4": {
-    group: "tissues",
-    chapter: "Chapter 2",
-    unitCode: "tissues3",
-    unitName: "Tissue Terms 3",
-    data: tissueTerms3
-  },
-  "5": {
-    group: "anatomyX",
-    chapter: "Chapter 1",
-    unitCode: "basic",
-    unitName: "Basic Anatomical Terms",
-    data: basicAnatomicalTerms
-  },
-  "6": {
-    group: "anatomyX",
-    chapter: "Chapter 2",
-    unitCode: "cells",
-    unitName: "Cell Parts",
-    data: cellParts
-  },
-  "7": {
-    group: "anatomyX",
-    chapter: "Chapter 2",
-    unitCode: "microscopes",
-    unitName: "Microscope Terms",
-    data: microscopeTerms
-  },
-  "8": {
-    group: "anatomyX",
-    chapter: "Chapter 2",
-    unitCode: "tissuesX",
-    unitName: "Tissue Terms",
-    data: tissueTermsX
-  },
-  "9": {
-    group: "tissues",
-    chapter: "Chapter 2",
-    unitCode: "tissues4",
-    unitName: "Tissue Terms 4",
-    data: tissueTerms4
-  },
-  "10": {
-    group: "nervous",
-    chapter: "Chapter 4",
-    unitCode: "nervous",
-    unitName: "Nervous Terms 1",
-    data: nervousTerms
-  },
-  "11": {
-    group: "nervous",
-    chapter: "Chapter 4",
-    unitCode: "nervous2",
-    unitName: "Nervous Terms 2",
-    data: nervousTerms2
-  }
+
 }
